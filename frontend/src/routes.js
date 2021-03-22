c43f64fa5fcb320af42863777424d8ad55d4fa3d
Hana <hayonleee@gmail.com>
2021-03-23 00:59:53 +0900

    [S04P23B103-11] feat: add AppHeader.vue

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,12 +1,7 @@
-import HelloWorld from '@/components/HelloWorld.vue';
 import StreamTest from '@/views/StreamTest.vue';
 
 export default [
-  {
-    path: '/',
-    name: 'HelloWorld',
-    component: HelloWorld,
-  },
+
   {
     path: '/stream',
     name: 'StreamTest',
