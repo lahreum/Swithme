2e1028ac27491e0e368de47b27ecad8ee0234e10
Hana <hayonleee@gmail.com>
2021-03-23 01:07:35 +0900

    [S04P23B103-0] Remove: delete HelloWorld.vue, logo

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,12 +1,6 @@
-import HelloWorld from '@/components/HelloWorld.vue';
 import StreamTest from '@/views/StreamTest.vue';
 
 export default [
-  {
-    path: '/',
-    name: 'HelloWorld',
-    component: HelloWorld,
-  },
   {
     path: '/stream',
     name: 'StreamTest',
