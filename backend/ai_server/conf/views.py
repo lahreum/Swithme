7c0efc7ad5f8b8e8c286a3fcd6e323f5c40c3cd5
Baekgol <changu13@naver.com>
2021-03-23 11:21:17 +0900

    Update: predict

--- backend/ai_server/conf/views.py
+++ backend/ai_server/conf/views.py
@@ -1,16 +1,20 @@
 import cv2
 import base64
 import numpy as np
-# from backend.ai_server.contents import train
+# from contents import train
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 
 @csrf_exempt
 def predict(request):
-    # byte 단위의 이미지를 3차원 ndarray로 변환
-    image_bytes = request.body
+    # byte 단위의 이미지 불러오기
+    image_bytes = request.FILES.get('data').file.getvalue()
+
+    # byte 단위의 이미지를 1차원 int형 ndarray로 변환
     image = np.frombuffer(image_bytes, dtype=np.uint8)
+
+    # 1차원에서 3차원 ndarray로 변환
     image = cv2.imdecode(image, cv2.IMREAD_COLOR)
 
     # ------------ predict ------------
@@ -24,4 +28,4 @@ def predict(request):
     # Base64 인코딩
     image_bytes = base64.b64encode(image_bytes)
 
-    return HttpResponse(image_bytes)
+    return HttpResponse(image_bytes, content_type='application/octet-stream')
