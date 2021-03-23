5b0ff163351308dc997a392bbbe2c3ae823f7ca5
Hana <hayonleee@gmail.com>
2021-03-23 10:56:32 +0900

    [S04P23B103-12] Feat: add AppFooter.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <v-app id="app">
     <app-header></app-header>
+    <app-footer></app-footer>
   </v-app>
 </template>
 
 <script>
 import AppHeader from './components/AppHeader.vue';
+import AppFooter from './components/AppFooter.vue';
 
 export default {
   components: {
     'app-header': AppHeader,
+    'app-footer': AppFooter,
   },
 };
 </script>
---
+++ frontend/src/components/AppFooter.vue
@@ -0,0 +1,52 @@
+<template>
+  <footer>
+    <v-row class="bg" align="center">
+      <v-col class="item" cols="4" align="center">
+        <img
+          src="../assets/img/logo_wh.png"
+          alt="swithme-logo"
+          style="width: 90%; height: auto;"
+        />
+      </v-col>
+      <v-col class="item">
+        <v-row
+          no-gutters
+          style="color: orange; margin-bottom: 5px; letter-spacing: -0.6px;"
+        >
+          학습지원센터 대표 전화: 042-750-8267 | 팩스: 042-750-8268
+        </v-row>
+        <v-row no-gutters style="margin-bottom: 5px; letter-spacing: -0.6px;">
+          상호명: (주)칼국수레인저 | 13269 대전광역시 유성구 학하서로121번길
+          45-22(덕명동, e스마트빌2)
+        </v-row>
+        <v-row no-gutters style="margin-bottom: 5px; letter-spacing: -0.6px;">
+          대표자: 이기호 | 사업자등록번호: 123-32-12345 | 개인정보 관리 책임자:
+          임찬규(changu13@naver.com)
+        </v-row>
+        <v-row
+          no-gutters
+          style="color: gray; font-size: 1.3rem; letter-spacing: -0.6px;"
+        >
+          Copyright ⓒ KALRA Edu.All rights reserved.
+        </v-row>
+      </v-col>
+    </v-row>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.bg {
+  background-color: black;
+  height: 250px;
+  margin-top: 150px;
+  padding-right: 20%;
+  padding-left: 20%;
+}
+.item {
+  color: white;
+}
+</style>
