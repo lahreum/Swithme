b347030f7231dda278a7feb991d723a019b7d3b9
이아름 <lar8224@gmail.com>
2021-03-23 14:15:24 +0900

    [S04P23B103-24]Feat: Develop InputBar

---
+++ frontend/src/components/common/InputBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-container>
+        <div>
+            <v-text-field
+                :placeholder="placeholder"
+                outlined
+                color="#673fb4"
+            ></v-text-field>
+        </div>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: "InputBar",
+    props: ['placeholder'],
+}
+</script>
+
+<style scoped>
+.v-text-field--outlined >>> fieldset {
+  border-color: #E0E0E0;
+  border-width: 4px;
+}
+</style>
