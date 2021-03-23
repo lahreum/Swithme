73b778b3b23c655ac12868ac1f6281fa44cf89c3
박봉현 <eeapbh@gmail.com>
2021-03-23 16:30:28 +0900

    [S04P23B103-13] Feat : AppBtnSmall.vue

---
+++ frontend/src/components/common/AppBtnSmall.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <v-btn rounded width="130px" height="50px" :color="btnColor">
+      <h3 :style="'color:' + btnNameColor">{{ btnName }}</h3>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["btnColor", "btnName", "btnNameColor"],
+};
+</script>
+
+<style></style>
