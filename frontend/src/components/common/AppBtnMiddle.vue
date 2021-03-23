e3ddaf59ee82657a6fddaba4a8ec4c6a320c662f
박봉현 <eeapbh@gmail.com>
2021-03-23 16:18:57 +0900

    [S04P23B103-13] Feat : AppBtnMiddle.vue

---
+++ frontend/src/components/common/AppBtnMiddle.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <v-btn rounded width="150px" height="50px" :color="btnColor">
+      <h3 :style="'color:' + btnNameColor">{{ btnName }}</h3>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["btnColor", "btnName", "btnNameColor"],
+};
+</script>
+
+<style></style>
