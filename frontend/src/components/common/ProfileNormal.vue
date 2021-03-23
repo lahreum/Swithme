59d9893cdc075d995f9b175f42dbebc62c6f4f68
박봉현 <eeapbh@gmail.com>
2021-03-23 14:30:04 +0900

    [S04P23B103-10] feat: add ProfileNormal.vue

---
+++ frontend/src/components/common/ProfileNormal.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <v-avatar size="70px">
+      <img src="@/assets/img/avatars/01.png" alt="Profile" />
+    </v-avatar>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
