420a212aa63a8e152071a2ae103d63878c624d26
이아름 <lar8224@gmail.com>
2021-03-23 17:21:13 +0900

    Docs: Add Wireframe

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <v-app>
     <app-header></app-header>
-    <comment-input-bar></comment-input-bar>
     <v-main>
       <router-view />
     </v-main>
@@ -12,13 +11,11 @@
 <script>
 import AppFooter from './components/include/AppFooter.vue';
 import AppHeader from './components/include/AppHeader.vue';
-import CommentInputBar from './components/common/CommentInputBar.vue';
 
 export default {
   components: {
     'app-header': AppHeader,
     'app-footer': AppFooter,
-    'comment-input-bar': CommentInputBar,
   },
 };
 </script>
