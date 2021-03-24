c88b42dedb6e780d07b08cadbbb6bb14ee738ede
Hana <hayonleee@gmail.com>
2021-03-24 15:09:19 +0900

    Feat: add path

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,7 +1,16 @@
 import StreamTest from '@/views/StreamTest.vue';
+import Tutorial from '@/views/HomeTutorial.vue';
 
 export default [
-
+  {
+    path: '/',
+    name: 'Main'
+  },
+  {
+    path: '/tutorial',
+    name: 'Tutorial',
+    component: Tutorial,
+  },
   {
     path: '/stream',
     name: 'StreamTest',
