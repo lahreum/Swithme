366babb56fb93ac12668ed3aee912700cc89b7be
박봉현 <eeapbh@gmail.com>
2021-03-25 22:40:54 +0900

    [S04P23B103-23] Update : Search.Bar.vue

--- frontend/src/components/common/SearchBar.vue
+++ frontend/src/components/common/SearchBar.vue
@@ -1,37 +1,40 @@
 <template>
-    <v-container>
-        <v-text-field
-            outlined
-            rounded
-            placeholder="검색"
-            dense 
-            prepend-inner-icon="mdi-magnify"
-            v-on:keyup.enter="search"
-            v-model="searchItem" color="#673fb4"
-            clearable
-        ></v-text-field>
-    </v-container>
+  <v-container style="">
+    <v-text-field
+      style="margin-top:7px; height:40px"
+      outlined
+      rounded
+      placeholder="검색"
+      dense
+      prepend-inner-icon="mdi-magnify"
+      v-on:keyup.enter="search"
+      v-model="searchItem"
+      color="#673fb4"
+      clearable
+    ></v-text-field>
+  </v-container>
 </template>
 
 <script>
 export default {
-    data: function() {
-        return {
-            searchItem: '',
-        }
+  data: function() {
+    return {
+      searchItem: "",
+    };
+  },
+  methods: {
+    search: function() {
+      if (this.searchItem !== "") {
+        // 무언가 입력되었을때
+        console.log(this.searchItem);
+        this.clearInput();
+      } else {
+        console.log("아무것도 입력되지 않았습니다");
+      }
     },
-    methods: {
-        search: function() {
-            if(this.searchItem !== '') {    // 무언가 입력되었을때
-                console.log(this.searchItem);
-                this.clearInput();
-            } else {
-                console.log('아무것도 입력되지 않았습니다');
-            }
-        },
-        clearInput: function () {
-            this.searchItem = '';
-        }
-    }
-}
+    clearInput: function() {
+      this.searchItem = "";
+    },
+  },
+};
 </script>
