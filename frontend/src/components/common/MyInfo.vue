18f5fe65518b2861e44f45b730a924e445438946
Hana <hayonleee@gmail.com>
2021-03-30 21:45:44 +0900

    Update: modify MyInfo.vue

--- frontend/src/components/common/MyInfo.vue
+++ frontend/src/components/common/MyInfo.vue
@@ -1,34 +1,47 @@
 <template>
   <v-row class="info-container" no-gutters align="center">
-    <div style="width: 200px;" align="center">
-      {{ profile }}
+    <div style="width: 250px;" align="center">
+      <profile-large
+        :src="require(`@/assets/img/avatars/iu.jpg`)"
+      ></profile-large>
     </div>
     <div class="info-right">
-      <v-row no-gutters>
-        <strong> {{ username }} </strong> 님, 오늘도 화이팅입니다!
+      <v-row no-gutters style=" font-size: 1.2rem;">
+        <strong> {{ user.username }} </strong> 님, 오늘도 화이팅입니다!
       </v-row>
       <v-row
         no-gutters
-        style="font-size: 3.3rem; font-weight: bold; margin-top: -10px; margin-bottom: -10px;"
+        style="font-size: 4rem; font-weight: bold; margin-top: -10px; "
       >
-        {{ studyTime }}
+        {{ user.studyTime }}
       </v-row>
       <div>
-        ( progress-bar )
+        <progress-bar :progressValue="30"></progress-bar>
       </div>
-      <v-row no-gutters style="font-size: 1.2rem;"> "{{ myGoal }}" </v-row>
+      <v-row no-gutters style="font-size: 1.2rem; margin-top: 10px;">
+        "{{ user.myGoal }}"
+      </v-row>
     </div>
   </v-row>
 </template>
 
 <script>
+import ProfileLarge from '@/components/common/ProfileLarge.vue';
+import ProgressBar from '@/components/common/ProgressBar.vue';
+
 export default {
+  components: {
+    'profile-large': ProfileLarge,
+    'progress-bar': ProgressBar,
+  },
   data: function() {
     return {
-      profile: '프로필 사진',
-      username: 'default',
-      studyTime: '9:36:33',
-      myGoal: '오늘 걷지 않으면, 내일 뛰어야 한다.',
+      user: {
+        profile: '프로필 사진',
+        username: 'default',
+        studyTime: '9:36:33',
+        myGoal: '오늘 걷지 않으면, 내일 뛰어야 한다.',
+      },
     };
   },
 };
@@ -36,13 +49,13 @@ export default {
 
 <style>
 .info-container {
-  height: 200px;
+  height: 240px;
   border-radius: 20px;
   background-color: #efefef;
   padding: 20px;
 }
 .info-right {
   padding-left: 20px;
-  letter-spacing: -0.7px;
+  letter-spacing: -1px;
 }
 </style>
