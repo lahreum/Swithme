a014b9d4a3c90a6d0f1f4ba18088c50b72b1d5cc
Hana <hayonleee@gmail.com>
2021-04-01 21:08:44 +0900

    Update: width of progress bar

--- frontend/src/views/user/Join.vue
+++ frontend/src/views/user/Join.vue
@@ -1,24 +1,27 @@
 <template>
-  <div>
-      <middle-nav v-bind:propsdata="navInfo"></middle-nav>
-      <progress-stepper ref="progress"></progress-stepper>
-      <hr align="center" class="joinForm">
-      <router-view></router-view>
-      
-  </div>
+  <v-container>
+    <middle-nav v-bind:propsdata="navInfo"></middle-nav>
+    <v-row justify="center">
+      <v-col cols="8">
+        <progress-stepper ref="progress"></progress-stepper>
+      </v-col>
+    </v-row>
+    <hr align="center" class="joinForm" />
+    <router-view></router-view>
+  </v-container>
 </template>
 
 <script>
-import "./user.css";
+import './user.css';
 import ProgressStepper from '@/components/common/ProgressStepper.vue';
 import MiddleNav from '@/components/include/MiddleNav.vue';
 
 export default {
-    components: {
-        'progress-stepper': ProgressStepper,
-        'middle-nav': MiddleNav,
-    },
-    data: function() {
+  components: {
+    'progress-stepper': ProgressStepper,
+    'middle-nav': MiddleNav,
+  },
+  data: function() {
     return {
       navInfo: [
         'sample1.jpg',
@@ -29,16 +32,14 @@ export default {
   },
   methods: {
     moveForward() {
-      this.$refs.progress.moveForward();  // 컴포넌트 내 메소드 실행(상단 프로그레스 바 전진)
+      this.$refs.progress.moveForward(); // 컴포넌트 내 메소드 실행(상단 프로그레스 바 전진)
       this.$router.push('/join-auth');
     },
     moveToHome() {
       this.$router.push('/');
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
