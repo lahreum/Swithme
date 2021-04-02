a5166d639fe5967e4c078e5eda4cd69f403c3ddb
Baekgol <changu13@naver.com>
2021-04-03 01:13:43 +0900

    Update: deploy

--- backend/rest_server/src/main/java/com/studywithme/StudywithmeApplication.java
+++ backend/rest_server/src/main/java/com/studywithme/StudywithmeApplication.java
@@ -3,14 +3,9 @@ package com.studywithme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.studywithme.config.SSHConnection;
-
 @SpringBootApplication
 public class StudywithmeApplication {
-//	private static SSHConnection sshConnection;
-
 	public static void main(String[] args) {
-//		sshConnection = new SSHConnection();
 		SpringApplication.run(StudywithmeApplication.class, args);
 	}
 
