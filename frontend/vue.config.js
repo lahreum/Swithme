383c07c40f4f4d02701728a9dd9f227b4b5aca8e
Baekgol <changu13@naver.com>
2021-04-03 22:44:26 +0900

    Update: deploy

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
-}
+  devServer: {
+    open: process.platform === 'darwin',
+    host: '0.0.0.0',
+    port: 8389,
+    https: true,
+    hotOnly: false,
+  },
+  transpileDependencies: ['vuetify'],
+};
