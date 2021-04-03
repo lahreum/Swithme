da42336cc33a89ae0822c6d9dd1d0f9c80d3f7e8
Baekgol <changu13@naver.com>
2021-04-03 23:58:50 +0900

    Update: deploy

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,10 +1,3 @@
 module.exports = {
-  devServer: {
-    https: {
-      key: fs.readFileSync('j4b103.p.ssafy.io.key'),
-      cert: fs.readFileSync('j4b103.p.ssafy.io.crt'),
-      ca: fs.readFileSync('rootca.crt'),
-    },
-  },
   transpileDependencies: ['vuetify'],
 };
